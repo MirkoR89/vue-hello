127e2bc46d7cdfb9c9e3bd1cba503434ed95c4a0
Mirko <mirko.rinelli@gmail.com>
2020-12-14 16:45:43 +0100

    Bonus: add image using data

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,15 @@
 //Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
-let root = new Vue({
+let text = new Vue({
   el: "#root",
   data: {
-    message: "Ciaone",
+    message: "Welcome to Vue.js"
   }
 })
 
-
-//Bonus: Aggiungere un’immagine presa anch’essa da un data
+//Bonus: Aggiungere un’immagine presa anch’essa da un data.
+let img = new Vue({
+  el: "#root2",
+  data: {
+    image: "https://hackernoon.com/hn-images/1*ACR0gj0wbx91V_xgURifWg.png"
+  }
+})
