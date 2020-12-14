30b4696ff7c5345df52da383e679de158d945c7c
Mirko <mirko.rinelli@gmail.com>
2020-12-14 15:53:37 +0100

    Print message with Vue date

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,10 @@
-//Stampare a schermo un messaggio all’interno di un h1, utilizzando i data. Bonus: Aggiungere un’immagine presa anch’essa da un data
+//Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+let root = new Vue({
+  el: "#root",
+  data: {
+    message: "Ciaone",
+  }
+})
+
+
+//Bonus: Aggiungere un’immagine presa anch’essa da un data
