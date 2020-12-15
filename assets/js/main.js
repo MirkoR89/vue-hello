511ade73b778298bda166be8694f2d59f0c76743
Mirko <mirko.rinelli@gmail.com>
2020-12-15 10:01:36 +0100

    Fixed inside one new Vue

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,9 @@
-//Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
 let text = new Vue({
   el: "#root",
   data: {
-    message: "Welcome to Vue.js"
-  }
-})
-
-//Bonus: Aggiungere un’immagine presa anch’essa da un data.
-let img = new Vue({
-  el: "#root2",
-  data: {
+    //Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+    message: "Welcome to Vue.js",
+    //Bonus: Aggiungere un’immagine presa anch’essa da un data.
     image: "https://hackernoon.com/hn-images/1*ACR0gj0wbx91V_xgURifWg.png"
   }
 })
